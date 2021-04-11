1c78e7678b77e79827a6abaa2162b112addef355
muneeb <muneeb.tariq@smarttra.de>
2021-04-11 13:48:56 +0200

    sort the dummy responses from the servie to verify the sorting

--- src/main/java/de/seven/senders/challenge/service/ComicService.java
+++ src/main/java/de/seven/senders/challenge/service/ComicService.java
@@ -1,9 +1,12 @@
 package de.seven.senders.challenge.service;
 
+import com.sun.syndication.io.FeedException;
 import de.seven.senders.challenge.model.Comic;
 import reactor.core.publisher.Flux;
 
+import java.io.IOException;
+
 public interface ComicService {
 
-    Flux<Comic> getAndSortComics();
+    Flux<Comic> getAndSortComics() throws IOException, FeedException;
 }
--- src/main/java/de/seven/senders/challenge/service/ComicServiceImpl.java
+++ src/main/java/de/seven/senders/challenge/service/ComicServiceImpl.java
@@ -1,8 +1,16 @@
 package de.seven.senders.challenge.service;
 
+import com.sun.syndication.io.FeedException;
 import de.seven.senders.challenge.model.Comic;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ComicServiceImpl implements ComicService {
@@ -18,8 +26,16 @@ public class ComicServiceImpl implements ComicService {
     }
 
     @Override
-    public Flux<Comic> getAndSortComics() {
-        return null;
+    public Flux<Comic> getAndSortComics() throws IOException, FeedException {
+
+        List<Comic> webcomicComics = webcomService.getComics();
+        List<Comic> pdlComics = pdlService.getComics();
+
+        List<Comic> comics = Stream.of(webcomicComics, pdlComics)
+                .flatMap(Collection::stream).sorted(Comparator.comparing(Comic::getPublishingDate).reversed())
+                .collect(Collectors.toList());
+
+        return Flux.fromIterable(comics);
 
 
 
